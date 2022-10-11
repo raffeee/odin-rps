5de68e18c8aabf4c1c5b26d472ef7a3e099d4f4b
Rafael Reyes <raffeee.reyes@gmail.com>
2022-10-12 00:54:41 +0800

    Implement 5-round RPS game

--- js/script.js
+++ js/script.js
@@ -12,26 +12,52 @@ function playRound(playerSelection, computerSelection) {
     `You Win! ${playerOption} beats ${computerSelection}`,
     `You Lose! ${computerSelection} beats ${playerOption}`
   ]
-
+  // 0 = Draw, 1 = PlayerWin; -1 = CompWin
   if (playerOption === computerSelection) {
-    return results[0];
+    console.log(results[0]);
+    return 0;
   } else if (playerOption === choice[0]) {
     if (computerSelection === choice[1]) {
-      return results[2]
+      console.log(results[2])
+      return -1
     } else {
-      return results[1]
+      console.log(results[1])
+      return 1
     }
   } else if (playerOption === choice[1]) {
     if (computerSelection === choice[2]) {
-      return results[2]
+      console.log(results[2])
+      return -1
     } else {
-      return results[1]
+      console.log(results[1])
+      return 1
     }
   } else if (playerOption === choice[2]) {
     if (computerSelection === choice[0]) {
-      return results[2]
+      console.log(results[2])
+      return -1
     } else {
-      return results[1]
+      console.log(results[1])
+      return 1
     }
   }
+}
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    let player = prompt("Rock, Paper, Scissors, shoot!")
+    let match = playRound(player, getComputerChoice())
+    if (match === 1) {
+      playerScore++
+    } else if (match === -1) {
+      computerScore++
+    }
+  }
+  return playerScore === computerScore
+  ? "No Contest"
+  : playerScore > computerScore
+    ? `Winner: Player!`
+    : `Winner: Computer!`
 }
